f492d7094e71997ade8cff5d56695bfaaedcb766
Siddhi Bhanushali <69195262+siddhi-244@users.noreply.github.com>
2021-06-22 15:39:37 +0530

    added 9

---
+++ Star_patterns/9.cpp
@@ -0,0 +1,29 @@
+/*
+*				*	
+	*		*		
+		*			
+	*		*		
+*				*	
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    //write your code here
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==j || i+j==n+1){
+                cout<<"*\t";
+            }
+            else{
+                cout<<"\t";
+            }
+        }
+        cout<<endl;
+    }
+
+}
